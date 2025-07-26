45213e7b5efc8ba22284d66090c0585f211a1103
tomeir2105 <tomeir2105@gmail.com>
2025-07-26 09:53:36 +0300

    Fixed music get pid

--- deploy-with-music.sh
+++ deploy-with-music.sh
@@ -3,15 +3,13 @@
 AUDIO_FILE="src/audio/audio.wav"
 
 if [ -f "$AUDIO_FILE" ]; then
-  aplay "$AUDIO_FILE" >/dev/null 2>&1
+    aplay "$AUDIO_FILE" >/dev/null 2>&1 &
 fi
 
 SOUND_PID=$!
 
 kubectl apply -f details-app.yaml
 
-sleep 15
-
-kill $SOUND_PID 2>/dev/null
+echo "Sound PID is $SOUND_PID"
 
 echo "Deployment complete."
